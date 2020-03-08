d91cc022be1c6152ce7d0fa53299291aacd72718
JONATHAN MUMMERY <johay.jjj@googlemail.com>
2020-03-08 22:41:36 +0000

    Ooops

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ var string = [];
 
 $('#Mapmap_listtier-1 > table > tbody > tr').each(function(index) { 
     var mapList = $(this).find('td').eq(3).text().split(",");
-    if ($(this).find('td').eq(2).find('a').filter($(".item_unique")).text()) {
+    if (!$(this).find('td').eq(2).find('a').filter($(".item_unique")).text()) {
         if(mapList[watchstones] >= minTier){string.push($(this).find('td').eq(2).text());} 
         else if (regions.indexOf($(this).find('td').eq(5).text()) !== -1) {string.push($(this).find('td').eq(2).text());}   
     } 
