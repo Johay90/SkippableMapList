3ffd4481cb8c11496c4c1afeb67f2ebe080ff4ce
JONATHAN MUMMERY <johay.jjj@googlemail.com>
2020-03-08 20:54:42 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
-var watchstones = 3;
-var minTier = 14;
-var regions = ["Glennach Cairns", "Haewark Hamlet"];
+// EDIT THE FOLLOWING 3 VARS.
+var watchstones = 3; // amount of watchstones
+var minTier = 14; // min tiers to show
+var regions = ["Glennach Cairns", "Haewark Hamlet"]; // this will show all maps in these regions, ignores minTier. 
 
+
+// don't edit below this line.
 var string = [];
 
 $('#Mapmap_listtier-1 > table > tbody > tr').each(function(index) { 
     var mapList = $(this).find('td').eq(3).text().split(",");
-    if (!$(this).find('td').eq(2).find('a').filter($(".item_unique")).text()) {
+    if ($(this).find('td').eq(2).find('a').filter($(".item_unique")).text()) {
         if(mapList[watchstones] >= minTier){string.push($(this).find('td').eq(2).text());} 
         else if (regions.indexOf($(this).find('td').eq(5).text()) !== -1) {string.push($(this).find('td').eq(2).text());}   
     } 
  });
 
+string = [...new Set(string)];
 console.log(string.toString().split(',').join("\r\n"));
